fc6768aa8b10e4e1ea85d58da2e103fd7ced8e4e
ctmbl <mabileau.clement@gmail.com>
2023-10-24 11:00:11 +0200

    Get nginx container ready for production

--- run_nginx.sh
+++ run_nginx.sh
@@ -1,1 +1,13 @@
+TERM="xterm-256color"
+#OFF=$(tput sgr0)
+OFF=""
+#RED=$(tput setaf 1)
+RED=""
+
+BUILD_FOLDER="./build/blog"
+
+if [ -z "$(ls -A $BUILD_FOLDER 2>/dev/null)" ]; then
+    echo "$RED[WARNING]$OFF '$BUILD_FOLDER' empty: nothing will be served by nginx server"
+fi
+
 nginx -g "daemon off;"

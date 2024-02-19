d5feb70447fc86a6a2937b48cfab578d5dee2d6f
ctmbl <mabileau.clement@gmail.com>
2024-02-19 22:35:06 +0100

    Handle leaf bundle articles in new_article.py script

--- scripts/new_article.py
+++ scripts/new_article.py
@@ -1,3 +1,4 @@
+import os
 import sys
 import re
 import requests
@@ -6,20 +7,31 @@ import yaml
 for file_path in sys.argv[1:]:
     # Check that this is an article file
     if re.match("^src/content/posts/.+\.md$", file_path):
-        # Read YAML Header
+        ## Read YAML Header
         with open(file_path, "r") as f:
             raw_txt = f.read()
             data = yaml.safe_load(raw_txt.split("---")[1])
 
-        # Get rid of python objects, only keep basic types
+        ## Get rid of python objects, only keep basic types
         for key in data:
             if type(data[key]) not in [int, str, float, bool]:
                 data[key] = str(data[key])
 
-        # Add URL info
-        file_name = file_path.split("/")[-1][:-3]
+        ## Add URL info:
+        # we have to deal with both possibilities of new article:
+        # - an article as a .md file which URL is the name
+        # - a leaf bundle article (https://gohugo.io/content-management/page-bundles/#leaf-bundles):
+        #   it's an article which name is the folder's name and body is in a index.md in this directory
+        dirname, basename = os.path.split(file_path)
+        if basename == "index.md":
+            # leaf bundle: name is directory name
+            file_name = os.path.basename(dirname)
+        else:
+            # direct article file: name is file name
+            file_name = basename[:-3] # get rid of the `.md`
+
         data["url"] = f"https://iscsc.fr/posts/{file_name}"
 
-        # Finally send Data
+        ## Finally send Data
         requests.post("http://iscsc.fr:8001/new-blog", json=data)
         print(file_path, file_name, data)

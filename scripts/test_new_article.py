859ead15299ff1ae3a4c1dfff57a4efcbbd1041d
ctmbl <mabileau.clement@gmail.com>
2024-05-05 18:45:27 +0200

    Fix lastly introduced regression test expected date formatting

--- scripts/test_new_article.py
+++ scripts/test_new_article.py
@@ -50,8 +50,8 @@ def test_new_article_file_upper_case(mock_requests_post):
         json={
             'title': 'article title',
             'summary': 'article name contains an upper case letter',
-            'date': '2024-05-05T12:00:00+02:00',
-            'lastUpdate': '2024-05-05T12:00:00+02:00',
+            'date': '2024-05-05 12:00:00+02:00',
+            'lastUpdate': '2024-05-05 12:00:00+02:00',
             'tags': "['some', 'tags']",
             'author': 'ctmbl',
             'draft': False,
